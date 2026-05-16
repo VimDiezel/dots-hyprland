eb6c8f514dc6e5396987e98d01f42e7a4af5b109
VimDiezel <Amr.Elhady32@gmail.com>
2026-05-16 19:57:55 +0300

    Porting to new Hyprland lua

--- dots/.config/hypr/hyprland/execs.lua
+++ dots/.config/hypr/hyprland/execs.lua
@@ -1,25 +1,37 @@
 -- put former exec-once commands inside the func and former exec commands outside
-hl.on("hyprland.start", function ()
+hl.on("hyprland.start", function()
+	-- Bar, wallpaper
+	hl.exec_cmd("$HOME/.config/hypr/hyprland/scripts/start_geoclue_agent.sh")
+	hl.exec_cmd("qs -c $qsConfig")
+	hl.exec_cmd("$HOME/.config/hypr/custom/scripts/__restore_video_wallpaper.sh")
 
-    -- Bar, wallpaper
-    hl.exec_cmd("$HOME/.config/hypr/hyprland/scripts/start_geoclue_agent.sh")
-    hl.exec_cmd("qs -c $qsConfig")
-    hl.exec_cmd("$HOME/.config/hypr/custom/scripts/__restore_video_wallpaper.sh")
+	-- Input method
+	hl.exec_cmd("~/apps/kanata-tray-linux")
 
-    -- Core components (authentication, lock screen, notification daemon)
-    hl.exec_cmd("gnome-keyring-daemon --start --components=secrets")
-    hl.exec_cmd("hypridle")
-    hl.exec_cmd("dbus-update-activation-environment --all")
-    hl.exec_cmd("sleep 1 && dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP") -- Some fix idk
+	-- Core components (authentication, lock screen, notification daemon)
+	hl.exec_cmd("gnome-keyring-daemon --start --components=secrets")
+	hl.exec_cmd("hypridle")
+	hl.exec_cmd("dbus-update-activation-environment --all")
+	hl.exec_cmd("sleep 1 && dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP") -- Some fix idk
 
-    -- Audio
-    hl.exec_cmd("easyeffects --hide-window --service-mode")
+	-- Audio
+	hl.exec_cmd("easyeffects --hide-window --service-mode")
 
-    -- Clipboard: history
-    --hl.exec_cmd("wl-paste --watch cliphist store")
-    hl.exec_cmd("wl-paste --type text --watch bash -c 'cliphist store && qs -c $qsConfig ipc call cliphistService update'")
-    hl.exec_cmd("wl-paste --type image --watch bash -c 'cliphist store && qs -c $qsConfig ipc call cliphistService update'")
+	-- Clipboard: history
+	--hl.exec_cmd("wl-paste --watch cliphist store")
+	hl.exec_cmd(
+		"wl-paste --type text --watch bash -c 'cliphist store && qs -c $qsConfig ipc call cliphistService update'"
+	)
+	hl.exec_cmd(
+		"wl-paste --type image --watch bash -c 'cliphist store && qs -c $qsConfig ipc call cliphistService update'"
+	)
 
-    -- Cursor
-    hl.exec_cmd("hyprctl setcursor Bibata-Modern-Classic 24")
+	-- Digital Vibrance
+	hl.exec_cmd("hyprshade on my-vibrance")
+
+	-- Pyprland
+	hl.exec_cmd("pypr")
+
+	-- Cursor
+	hl.exec_cmd("hyprctl setcursor Bibata-Modern-Classic 24")
 end)
